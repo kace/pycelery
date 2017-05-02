cd2a6b8ff061984daf22b993499f4a44b9061802
Kace <kace42205@gmail.com>
2017-05-02 02:15:12 -0700

    Updating import pathing.

--- execute_test.py
+++ execute_test.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
-import imp
+import os, sys
 from automated_test import automatedTest 
 from celery import Celery
 
 app = Celery('tasks', broker = 'pyamqp://guest@localhost//')
 
 def load_test(testPath):
-    if testPath.endswith('.py'):
-        testPath = testPath[:-3]
-    module = __import__(testPath)
+   
+    moduleName = testPath.strip()
+
+    moduleDir = os.path.dirname(moduleName)
+    if moduleDir not in sys.path:
+        sys.path.insert(0, moduleDir)
+
+    moduleName = os.path.basename(moduleName)
+    if moduleName.endswith('.py'):
+        moduleName = moduleName[:-3]
+
+    module = __import__(moduleName)
+    sys.path.pop(sys.path.index(moduleDir))
+
     objs = dir(module)
     for objname in objs:
         obj = getattr(module,objname)
